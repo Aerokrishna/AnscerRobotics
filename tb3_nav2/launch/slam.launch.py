7d4e017504e075533c0ccec0274dabeeb96ab57e
Krishnapranav <krishnapranav.221me227@nitk.edu.in>
2025-09-26 15:51:40 +0530

    turtlebot3 added, nav2 setup done

---
+++ tb3_nav2/launch/slam.launch.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, PathJoinSubstitution
+
+def generate_launch_description():
+    
+    slam_params_path = os.path.join(
+        get_package_share_directory('tb3_nav2'),  # Change this to your package
+        'params',
+        'slam_params.yaml'
+    )
+
+    return LaunchDescription([
+       
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[slam_params_path]
+        )
+
+    ])
